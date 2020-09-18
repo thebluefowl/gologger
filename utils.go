b40f91d5f5679ab64cfe61992eac8818ab36206c
Vishnu Jayadevan <vishnu@deepsource.io>
2020-09-18 17:35:36 +0530

    Security

--- utils.go
+++ utils.go
@@ -12,7 +12,7 @@ import (
 // GetLogWriter returns an io.Writer for log
 // if already  created returns the same otherwise creates a new buffered Writer and returns
 func GetLogWriter() (io.Writer, error) {
-	file, err := os.OpenFile(fmt.Sprintf("%s/%s.log", logDirectory, processName), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0640)
+	file, err := os.OpenFile(fmt.Sprintf("%s/%s.log", logDirectory, processName), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		log.Println("[LOGGER]Error opening file", err.Error())
 		return nil, err
